5fe0985706e05b5866c6596aa8ce82f2b423414e
sainadhreddy92 <sainadhreddy92@gmail.com>
2015-06-22 23:24:44 -0700

    email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,20 +14,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
-    		@user = User.new(user_params)    # Not the final implementation!
+    		@user = User.new(params[:user])    # Not the final implementation!
     		if @user.save
       			flash[:success] = "Welcome to the CSG Mentoring Tool!"
       			redirect_to @user
-    		else
-			flash[:notice] = "#Error regsitering."
-      			render 'new'
+    		else 
+			flash[:notice] = "Error regsitering."
+			
+			if params[:user][:user_role]
+                          render :partial => 'users/mentor', :layout => 'partial'
+			 else
+  		          render :new
+     			end
     	         end
         end
 
-	private
-	##Strong Parametres
-	def user_params
-	 params.require(:user).permit(:user_role, :cisco_email, :current_group, :work_city, :conflict_resolution, :customer_know_how, :personal_branding,
-			:leading_change, :exec_acheive_results, :align_and_influence, :managing_without_authority, :career_exploration, :win_win_negotiation, :effective_communication, :think_out_box, :tech_know, :other, :tech_areas, :other_areas)          
-	end
+	
+	
 end
--- app/models/user.rb~
+++ app/models/user.rb~
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-  
+	attr_accessible :user_role, :cisco_email, :current_group, :work_city, :conflict_resolution, :customer_know_how, :personal_branding,
+			:leading_change, :exec_acheive_results, :align_and_influence, :managing_without_authority, :career_exploration, :win_win_negotiation, :effective_communication, :think_out_box, :tech_know, :other, :tech_areas, :other_areas         
+	
+	email_regex = /\A[\w+\-.]+@cisco.com/i
+
+        validates :cisco_email, :presence   => true,
+                  :format     => { :with => email_regex,}
+	validates :work_city, :presence => true
 
 end
