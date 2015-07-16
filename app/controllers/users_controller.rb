4ee392846cec42c41945f9ef0f4ad39f13e57ce3
sainadhreddy92 <sainadhreddy92@gmail.com>
2015-07-15 20:38:53 -0700

    binding to ip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 	def create
     		@user = User.new(params[:user])    # Not the final implementation!
     		if @user.save
-                        #UserMailer.notify(@user).deliver
+                        UserMailer.notify(@user).deliver
     			log_in @user
       			flash[:notice] = "User Created Successfully!"
       			redirect_to @user
